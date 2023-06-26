f12d2c5941867ee72958dd7c0b02b861725d39d3
huyhdq <huyhdq@ftech.ai>
2023-06-26 09:14:46 +0700

    Initial Commit

---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Ha Duyen Quang Huy on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
